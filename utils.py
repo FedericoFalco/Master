bc132b540b56fdfdec17da28ea21c067aa3e5c54
FedericoFalco <federico_falco@outlook.it>
2024-11-16 11:57:21 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,7 +2,13 @@ import pandas as pd
 import numpy as np
 
 class Undefined:
-    #constructor method for ensuring that, when instanced,
-    #a new object of this class has its own inizialized data
+    #constructor method for ensuring that, when instanced, a new object of this class has its own inizialized data
     def __init__(self,data):
         self.data = data
+
+def merge_ratings_movies(df_ratings,df_movies)
+# left join between ratings and movies to preserve all the ratings rows
+    df_ratings = pd.merge(df_ratings, df_movies, how='left', on='movieId')
+    del df_ratings['movieId']
+    return df_ratings
+    
