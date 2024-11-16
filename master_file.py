7df2742f021771c88ea7915d0dc115077cd847c2
FedericoFalco <federico_falco@outlook.it>
2024-11-16 11:38:49 +0100

    Update master_file.py

--- master_file.py
+++ master_file.py
@@ -5,7 +5,7 @@ import Undefined from class_script
 def get_dataset(dataset):
   # retrieving data in a Pandas df
   if dataset == 'facebook_book':
-    train_set = pd.read_csv('../data/dataset/hetrec2011_lastfm_2k/splitting/0/train_with_name.tsv', sep="\t",
+    train_set = pd.read_csv('../data/dataset/facebook_book/trainingset_with_name.tsv', sep="\t",
                             header=None, names=['userId', 'bookId', 'rating', 'name'],
                             usecols=['userId', 'bookId', 'rating', 'name'])
     # calling the class defined in the class_script
@@ -16,5 +16,7 @@ def get_dataset(dataset):
                             usecols=['userId', 'artistId', 'weight', 'name'])
     trainset = Undefined(train_set)
   elif dataset == 'ml_small_2018':
-    train_set = pd.read_csv(
+    # assume to having picked EXP 1, with only recommendations to be asked to ChatGPT
+    ratings = pd.read_csv('../data/dataset/ml_small_2018/splitting/0/subset_train_230.tsv'
+    train_set = pd.read_csv('../data
     
