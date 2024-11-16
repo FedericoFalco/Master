8fef23e035efa4a4339595f58fc01092d981d18e
FedericoFalco <federico_falco@outlook.it>
2024-11-16 19:46:07 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from openai import OpenAI
 
 class Undefined:
     #constructor method for ensuring that, when instanced, a new object of this class has its own inizialized data
@@ -12,3 +13,26 @@ def merge_ratings_movies(df_ratings,df_movies)
     del df_ratings['movieId']
     return df_ratings
     
+class Open_AI():
+    # constructor method for ensuring that, when instanced, a new object of this class has its own model
+    def __init__(self,model):
+        self.model = model
+
+    def question(self,message):
+        # calling the OpenAI api
+        client = OpenAI(api_key = 'tbd')
+        # setting the conversation
+        answer = client.chat.completions.create(model=self.model, message=[{"role": "system", "content": "
+                        "Given a user, as a Recommender System, please provide only the name of the top 50 recommendations."},
+                        {"role": "user", "content": message}],
+                                                temperature=0,
+                                                max_tokens=500,
+                                                top_p=1,
+                                                frequence_penalty=0,
+                                                presence_penalty=0)
+        return answer
+                                                
+                                                                           
+                                                                            
+                                                            
+        
