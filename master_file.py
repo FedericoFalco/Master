322cb1e91629066024028edbe216a3b51746a9c1
FedericoFalco <federico_falco@outlook.it>
2024-11-18 23:23:00 +0100

    Update master_file.py

--- master_file.py
+++ master_file.py
@@ -10,22 +10,22 @@ def chatGPT_recommender(model,dataset):
   # retrieving data in a Pandas df
   if dataset == 'facebook_book':
     train_set = pd.read_csv('../trainingset_with_name.tsv', sep="\t",
-                            header=None, names=['userId', 'bookId', 'rating', 'name'],
-                            usecols=['userId', 'bookId', 'rating', 'name'])
+                            header=None, names=['userID', 'bookID', 'rating', 'name'],
+                            usecols=['userID', 'bookID', 'rating', 'name'])
     # calling the class defined in the class_script
     trainset = Undefined(train_set)
   elif dataset == 'hetrec2011_lastfm_2k':
     train_set = pd.read_csv('../train_with_name.tsv', sep="\t",
-                            header=None, names=['userId', 'artistId', 'weight', 'name', 'url', 'pictureURL'],
-                            usecols=['userId', 'artistId', 'weight', 'name'])
+                            header=None, names=['userID', 'artistID', 'weight', 'name', 'url', 'pictureURL'],
+                            usecols=['userId', 'artistID', 'weight', 'name'])
     trainset = Undefined(train_set)
   elif dataset == 'ml_small_2018':
     # assume to having picked EXP 1, with only recommendations to be asked to ChatGPT
     ratings = pd.read_csv('../subset_train_230.tsv', sep="\t",
-                            header=None, names=['userId', 'movieId', 'rating'])
+                            header=None, names=['userID', 'movieID', 'rating'])
     movies = pd.read_csv('../movies.csv', sep=',', 
-                            header=None, names=['movieId','title','genre'],
-                            usecols=['movieId','title'])
+                            header=None, names=['movieID','title','genre'],
+                            usecols=['movieID','title'])
     train_set = merge_ratings_movies(ratings,movies)
     trainset = Undefined(train_set)
   else:
@@ -35,6 +35,7 @@ def chatGPT_recommender(model,dataset):
 # iterating over users included in the dataset
 for user in trainset['userId'].unique():
   message = ''
-  if dataset = 
+  if dataset = 'facebook_book'
+    
 
     
