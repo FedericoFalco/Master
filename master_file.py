b5789c48e86c66223f2394a7f5e17a8109367d03
FedericoFalco <federico_falco@outlook.it>
2024-11-20 23:01:31 +0100

    Update master_file.py

--- master_file.py
+++ master_file.py
@@ -1,13 +1,18 @@
 import pandas as pd
 import numpy as np
-import Undefined, merge_ratings_movies from utils
+import * from utils
 
 def chatGPT_recommender(model,dataset):
   # using the Open_AI function created to call the ChatGPT API
   if model == 'gpt-3.5-turbo-1106':
-    recommender = 
+    recommender = Open_AI('gpt-3.5-turbo-1106')
+  elif model == 'gpt-4-0125-preview':
+    recommender = Open_AI('gpt-4-0125-preview')
+  else:
+    print('no valid model)
+    exit()
     
-  # retrieving data in a Pandas df
+  # retrieving data in a pandas df
   if dataset == 'facebook_book':
     train_set = pd.read_csv('../trainingset_with_name.tsv', sep="\t",
                             header=None, names=['userID', 'bookID', 'rating', 'name'],
@@ -35,7 +40,12 @@ def chatGPT_recommender(model,dataset):
 # iterating over users included in the dataset
 for user in trainset['userId'].unique():
   message = ''
-  if dataset = 'facebook_book'
+  if dataset == 'facebook_book':
+    message = train_set.book_read(user)
+  elif dataset == 'hetrec2011_lastfm_2k':
+    message = train_set.artists_listened(user)
+  elif dataset == 'ml_small_2018':
+    message = train_set.movie_rated(user)
     
 
     
